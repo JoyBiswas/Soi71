3d107f8c27fdd2ea213f05512cb0cf4f523e6583
Tiger Soft BD <retina@macbooks-macbook-pro.local>
2018-07-21 11:31:21 +0600

    begging of project

---
+++ Soi71/Controller/SoiHomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  SoiHomeVC.swift
+//  Soi71
+//
+//  Created by RIO on 7/21/18.
+//  Copyright © 2018 RIO. All rights reserved.
+//
+
+import UIKit
+
+class SoiHomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
